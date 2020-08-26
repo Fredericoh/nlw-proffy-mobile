16056f39be96b42c95e6196118eb8013adbe465f
Frederico Hernany <fredericoh@gmail.com>
2020-08-25 23:24:38 -0300

    first commit

---
+++ src/pages/Favorites/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0F0F0',
+  },
+
+  teacherList: {
+    marginTop: -50, 
+  }
+})
+
+export default styles;
